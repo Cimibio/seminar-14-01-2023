2c0c6a5d0a1f3f5e6a94ffd4ff52a543aae7f613
AndreySh <shingaryovan@gmail.com>
2023-01-14 23:57:51 +1200

    добавлен расчет квадрата с вводом числа вручную

--- exmpl001/Program.cs
+++ exmpl001/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = new Random().Next(1,100);;
+//int a = new Random().Next(1,100);
+//Console.Write("Квадрат числа ");
+//Console.Write(a);
+//Console.Write(" = ");
+//Console.Write(a*a);
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Квадрат числа ");
 Console.Write(a);
 Console.Write(" = ");
