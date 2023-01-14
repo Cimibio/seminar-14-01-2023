fa872fd4c9dba00145a70d5a49d73b230e6c2601
AndreySh <shingaryovan@gmail.com>
2023-01-15 01:02:08 +1200

    добавлен пример с выводом значений от -N до N с примерами циклов for да while

---
+++ exmpl003/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число");
+int n = int.Parse(Console.ReadLine());
+//for (int i = -n; i <= n; i++ )
+//{
+//    Console.WriteLine(i);
+//}
+int i = -n;
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i++;
+}
