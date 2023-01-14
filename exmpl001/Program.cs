eda685b32ceb13002ff9d301bbca66236f3fddf7
AndreySh <shingaryovan@gmail.com>
2023-01-14 23:45:25 +1200

    добавлен расчет квадрата рандомного числа

---
+++ exmpl001/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = new Random().Next(1,100);;
+Console.Write("Квадрат числа ");
+Console.Write(a);
+Console.Write(" = ");
+Console.Write(a*a);
