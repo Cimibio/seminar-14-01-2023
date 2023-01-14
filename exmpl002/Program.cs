d89add35b92e2d645538a89abc51f315b3f1cb18
AndreySh <shingaryovan@gmail.com>
2023-01-15 00:31:39 +1200

    добавлено сравнение на равенство

--- exmpl002/Program.cs
+++ exmpl002/Program.cs
@@ -3,11 +3,17 @@ Console.WriteLine("введите первое число: ");
 int n1 = int.Parse(Console.ReadLine());
 Console.WriteLine("введите второе число: ");
 int n2 = int.Parse(Console.ReadLine());
-if (n1 > n2)
+if (n1 == n2)
+{
+    Console.WriteLine("число " + n1 + " равно числу " + n2);
+}
+else if (n1 > n2)
 {
     Console.WriteLine("число " + n1 + " больше числа " + n2);
 }    
 else
+
 {
     Console.WriteLine("число " + n2 + " больше числа " + n1);
 }
+
