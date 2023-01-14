0a0aa9c8c50e56bd2ce33e7ad16acd71e1f104c3
AndreySh <shingaryovan@gmail.com>
2023-01-15 00:19:10 +1200

    добавлен пример с сравниваением двух чисел и выводом на экран

---
+++ exmpl002/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 > n2)
+{
+    Console.WriteLine("число " + n1 + " больше числа " + n2);
+}    
+else
+{
+    Console.WriteLine("число " + n2 + " больше числа " + n1);
+}
